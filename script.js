a1d20d59618af5092b05bc94536a6c0ac486792b
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-14 20:26:51 +0100

    Updating the form alert

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const form = document.getElementById('form')
 
 const onSubmit = () => {
 	if (form.checkValidity()) {
-		alert('we have received your inquiry and we will get back to you shortly')
+		alert(
+			'Thank you, We have received your inquiry and we will get back to you shortly.'
+		)
 	}
 }
