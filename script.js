5283158ae68a1b595e89320f57a0ab38820ddb3f
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-07-14 20:23:22 +0100

    Updating the form and Adding the submit

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-	$(".btn").click(function () {
-		$("nav ul").slideToggle(1500);
-	});
-});
+	$('.btn').click(function () {
+		$('nav ul').slideToggle(1500)
+	})
+})
+
+const form = document.getElementById('form')
+
+const onSubmit = () => {
+	if (form.checkValidity()) {
+		alert('we have received your inquiry and we will get back to you shortly')
+	}
+}
